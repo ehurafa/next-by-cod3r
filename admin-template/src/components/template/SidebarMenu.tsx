d31a3bdaf7eaac0358ce2d8b8cada2cb8ca19ed5
Rafael Gomes <rafarfl2010@gmail.com>
2026-01-28 17:41:25 -0300

    feat(component): adding Logo component

--- admin-template/src/components/template/SidebarMenu.tsx
+++ admin-template/src/components/template/SidebarMenu.tsx
@@ -1,9 +1,15 @@
 import { BellIcon, HomeIcon, SettingsIcon } from "../icons"
+import Logo from "./Logo"
 import MenuItem from "./MenuItem"
 
 export default function SidebarMenu(props) {
     return (
         <aside>
+            <div className={`
+                flex flex-colum items-center justify-center h-20 w-20 bg-gradient-to-r from-indigo-500 to-purple-800
+            `}>
+                <Logo />
+            </div>
             <ul>
                 <MenuItem url="/" text="Início" icon={HomeIcon} />
                 <MenuItem url="/ajustes" text="Ajustes" icon={SettingsIcon} />
