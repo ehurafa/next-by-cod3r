f614ecb2a8f121d8ae0433f2f8746a4d1eb138c9
Rafael Gomes <rafarfl2010@gmail.com>
2026-01-28 21:36:35 -0300

    feat(style): color scheme

--- admin-template/src/components/template/MenuItem.tsx
+++ admin-template/src/components/template/MenuItem.tsx
@@ -13,16 +13,16 @@ export default function MenuItem(props: MenuItemProps) {
     return (
         <li
             onClick={props.onClick}
-            className={`hover:bg-gray-100 cursor-pointer`}>
+            className={`hover:bg-gray-100 dark:hover:bg-gray-800 cursor-pointer`}>
             {
                 props.url ?
-                    (<Link href={props.url} className={`flex flex-col justify-center items-center h-20 w-full text-gray-600 ${props.className}`}>
+                    (<Link href={props.url} className={`flex flex-col justify-center items-center h-20 w-full text-gray-600 dark:text-gray-200 ${props.className}`}>
                         {props.icon}
                         <span className={`text-xs font-light`}>
                             {props.text}
                         </span>
                     </Link>) :
-                    <a href={props.url} className={`flex flex-col justify-center items-center h-20 w-full text-gray-600 ${props.className}`}>
+                    <a href={props.url} className={`flex flex-col justify-center items-center h-20 w-full text-gray-600 dark:text-gray-200 ${props.className}`}>
                         {props.icon}
                         <span className={`text-xs font-light`}>
                             {props.text}
--- admin-template/src/components/template/SidebarMenu.tsx
+++ admin-template/src/components/template/SidebarMenu.tsx
@@ -8,6 +8,7 @@ export default function SidebarMenu(props) {
     return (
         <aside className={`
             flex flex-col
+            dark:bg-gray-900
         `}>
             <div className={`
                 flex flex-colum items-center justify-center h-20 w-20 bg-gradient-to-r from-indigo-500 to-purple-800
@@ -25,7 +26,7 @@ export default function SidebarMenu(props) {
                     onClick={() => console.log("Logout")}
                     text="Sair"
                     icon={LogoutIcon}
-                    className={`text-red-600 hover:bg-red-400 hover:text-white`}
+                    className={`text-red-600 hover:bg-red-400 dark:hover:bg-red-400 hover:text-white dark:hover:text-white`}
                 />
             </ul>
         </aside>
