dbe1b4469058bf33ff2482fbd928ecbc95420177
Rafael Gomes <rafarfl2010@gmail.com>
2026-02-04 21:25:33 -0300

    feat(style): styling auth elements

--- admin-template/src/app/authentication/page.tsx
+++ admin-template/src/app/authentication/page.tsx
@@ -10,23 +10,62 @@ export default function AuthenticationPage() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
+    function send() {
+        if (mode === "login") {
+            console.log("Logando")
+        } else {
+            console.log("Cadastrando")
+        }
+    }
+
     return (
-        <div>
-            <h1>Autenticação</h1>
-            <AuthInput
-                label="Email"
-                type="email"
-                value={email}
-                changeValue={setEmail}
-                required
-            />
-            <AuthInput
-                label="Senha"
-                type="password"
-                value={password}
-                changeValue={setPassword}
-                required
-            />
+        <div className={`
+            flex flex-col h-screen items-center justify-center
+        `}>
+            <div className={`
+                w-1/2
+            `}>
+                <h1 className={
+                    `text-xl font-bold mb-5 
+                    `
+                }>{mode === "login" ? "Entre com a sua conta" : "Cadastre-se na plataforma"}</h1>
+                <AuthInput
+                    label="Email"
+                    type="email"
+                    value={email}
+                    changeValue={setEmail}
+                    required
+                />
+                <AuthInput
+                    label="Senha"
+                    type="password"
+                    value={password}
+                    changeValue={setPassword}
+                    required
+                />
+                <button
+                    className={`
+                        w-full bg-indigo-500 hover:bg-indigo-400 text-white rounded-lg px-4 py-3 mt-6 cursor-pointer
+                        `}
+                    onClick={send}
+                >
+                    {mode === "login" ? "Entrar" : "Cadastrar"}
+                </button>
+
+                <hr className={`
+                    my-6 border-gray-300 w-full
+                `}/>
+
+                <button
+                    className={`
+                        w-full bg-red-500 hover:bg-red-400 text-white rounded-lg px-4 py-3 cursor-pointer
+                        `}
+                    onClick={send}
+                >
+                    Entrar com o Google
+                </button>
+    
+            </div>
         </div>
     )
 }
--- admin-template/src/components/auth/AuthInput.tsx
+++ admin-template/src/components/auth/AuthInput.tsx
@@ -11,13 +11,16 @@ interface AuthInputProps {
 
 export default function AuthInput(props: AuthInputProps) {
     return props.whenShouldYouNotRender ? null :(
-        <div className="flex flex-col">
+        <div className="flex flex-col mt-4">
             <label>{props.label}</label>
             <input
                 type={props.type ?? "text"}
                 value={props.value}
                 onChange={e => props.changeValue?.(e.target.value)}
                 required={props.required}
+                className={`
+                    px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:outline-none focus:bg-white
+                    `}
             />
         </div>
     )
