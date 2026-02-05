34f61b2f6ae0a806796d8afeb06f1ee6801c0639
Rafael Gomes <rafarfl2010@gmail.com>
2026-02-04 22:14:04 -0300

    feat(style): styling register and login view

--- admin-template/src/app/authentication/page.tsx
+++ admin-template/src/app/authentication/page.tsx
@@ -20,13 +20,20 @@ export default function AuthenticationPage() {
 
     return (
         <div className={`
-            flex flex-col h-screen items-center justify-center
+            flex h-screen items-center justify-center
         `}>
+            <div className={`hidden md:block md:w-1/2 lg:w-2/3`}>
+                <img
+                    src="https://source.unsplash.com/random"
+                    alt="Imagem da tela de Autenticação"
+                    className={`h-screen w-full object-cover`}    
+                />
+            </div>
             <div className={`
-                w-1/2
+                w-full md:w-1/2 lg:w-1/3 m-10
             `}>
                 <h1 className={
-                    `text-xl font-bold mb-5 
+                    `text-3xl font-bold mb-5 
                     `
                 }>{mode === "login" ? "Entre com a sua conta" : "Cadastre-se na plataforma"}</h1>
                 <AuthInput
@@ -64,6 +71,18 @@ export default function AuthenticationPage() {
                 >
                     Entrar com o Google
                 </button>
+
+                { mode === "login" ? (
+                    <p className="mt-8">
+                        Novo por aqui?
+                        <a onClick={() => setMode("register")} className="text-blue-500 hover:text-blue-700 font-semibold cursor-pointer ml-1">Crie uma conta gratuitamente</a>
+                    </p>
+                ) :  (
+                     <p className="mt-8">
+                        Já faz parte da nossa comunidade?
+                        <a onClick={() => setMode("login")} className="text-blue-500 hover:text-blue-700 font-semibold cursor-pointer ml-1">Entre com as suas credenciais</a>
+                    </p>
+                ) }
     
             </div>
         </div>
